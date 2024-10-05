0ab2925d473dc8509ab1822076d655686a9b574c
TcD66 <21004295@st.vlute.edu.vn>
2024-10-05 22:53:15 +0700

    .csdl

---
+++ tmdt_db.sql
@@ -0,0 +1,489 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.2
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost:3306
+-- Generation Time: Oct 05, 2024 at 10:50 PM
+-- Server version: 5.7.24
+-- PHP Version: 8.1.0
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `tmdt_db`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `baidang`
+--
+
+CREATE TABLE `baidang` (
+  `MaBD` int(11) NOT NULL,
+  `MaTK` int(11) DEFAULT NULL,
+  `MaSP` int(11) DEFAULT NULL,
+  `AnhBD` text,
+  `NoiDungBD` text,
+  `NgayTaoBD` text
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `baocao`
+--
+
+CREATE TABLE `baocao` (
+  `MaBC` int(11) NOT NULL,
+  `NoiDungBC` text,
+  `TongDoanhThu` text,
+  `NgayTaoBC` text,
+  `MaTK` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `binhluan`
+--
+
+CREATE TABLE `binhluan` (
+  `MaBL` int(11) NOT NULL,
+  `MaTK` int(11) DEFAULT NULL,
+  `MaSP` int(11) DEFAULT NULL,
+  `DanhGia` text,
+  `AnhBL` text,
+  `NoiDungDG` text,
+  `NgayTaoBL` text
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `chitietdonhang`
+--
+
+CREATE TABLE `chitietdonhang` (
+  `MaCTDH` int(11) NOT NULL,
+  `MaDH` int(11) DEFAULT NULL,
+  `MaSP` int(11) DEFAULT NULL,
+  `SoLuong` text,
+  `Gia` text
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `chitietsanpham`
+--
+
+CREATE TABLE `chitietsanpham` (
+  `MaCTSP` int(11) NOT NULL,
+  `MaSP` int(11) DEFAULT NULL,
+  `ThongSoKyThuat` text,
+  `NhaCungCap` text,
+  `BaoHanh` text,
+  `XuatXu` text
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `danhmucsanpham`
+--
+
+CREATE TABLE `danhmucsanpham` (
+  `MaDM` int(11) NOT NULL,
+  `TenDM` text,
+  `AnhDM` text,
+  `TrangThaiDM` text
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `donhang`
+--
+
+CREATE TABLE `donhang` (
+  `MaDH` int(11) NOT NULL,
+  `TrangThaiDH` text,
+  `TongTien` text,
+  `DiaChiGiaoHang` text,
+  `NgayTaoDH` text,
+  `PhuongThucThanhToan` int(11) DEFAULT NULL,
+  `MaTK` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `giohang`
+--
+
+CREATE TABLE `giohang` (
+  `MaGH` int(11) NOT NULL,
+  `MaTK` int(11) DEFAULT NULL,
+  `MaSP` int(11) DEFAULT NULL,
+  `SoLuong` text
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `hangsanxuat`
+--
+
+CREATE TABLE `hangsanxuat` (
+  `MaHSX` int(11) NOT NULL,
+  `TenHSX` text,
+  `AnhHSX` text,
+  `TrangThaiHSX` text
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `khuyenmai`
+--
+
+CREATE TABLE `khuyenmai` (
+  `MaKM` int(11) NOT NULL,
+  `MaTK` int(11) DEFAULT NULL,
+  `TenKM` text,
+  `DieuKien` text,
+  `PhanTramGiam` text,
+  `GiaTriToiDa` text,
+  `NgayBD` text,
+  `NgayKT` text,
+  `SoLanSuDung` text
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `lichsutrangthaidh`
+--
+
+CREATE TABLE `lichsutrangthaidh` (
+  `MaLS` int(11) NOT NULL,
+  `MaDH` int(11) DEFAULT NULL,
+  `TrangThaiCu` text,
+  `TrangThaiMoi` text,
+  `NgayThaiDoi` text
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `phanquyen`
+--
+
+CREATE TABLE `phanquyen` (
+  `MaPQ` int(11) NOT NULL,
+  `MaTK` int(11) DEFAULT NULL,
+  `QLDonHang` text,
+  `QLSanPham` text,
+  `QLTaiKhoan` text,
+  `QLBinhLuan` text,
+  `QLBaoCao` text
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `phuongthucthanhtoan`
+--
+
+CREATE TABLE `phuongthucthanhtoan` (
+  `MaPTTT` int(11) NOT NULL,
+  `TenPTTT` text
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `sanpham`
+--
+
+CREATE TABLE `sanpham` (
+  `MaSP` int(11) NOT NULL,
+  `TenSP` text,
+  `AnhSP` text,
+  `GiaBan` text,
+  `SoLuongTonKho` text,
+  `NgayTaoSP` text,
+  `TrangThaiSP` text,
+  `MoTaChiTiet` text,
+  `ThoiGianBaoHanh` text,
+  `HangSanXuat` int(11) DEFAULT NULL,
+  `DanhMucSP` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `taikhoan`
+--
+
+CREATE TABLE `taikhoan` (
+  `MaTK` int(11) NOT NULL,
+  `TenDangNhap` text,
+  `MatKhau` text,
+  `VaiTro` text,
+  `AnhDaiDien` text,
+  `Email` text,
+  `NgaySinh` text,
+  `GioiTinh` text,
+  `SDT` text,
+  `NgayTaoTK` text,
+  `NgaySuaDoi` text,
+  `DiaChi` text
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `thongbao`
+--
+
+CREATE TABLE `thongbao` (
+  `MaTB` int(11) NOT NULL,
+  `NoiDungTB` text,
+  `NgayTaoTB` text,
+  `MaTK` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `thongtinvanchuyen`
+--
+
+CREATE TABLE `thongtinvanchuyen` (
+  `MaVC` int(11) NOT NULL,
+  `MaDH` int(11) DEFAULT NULL,
+  `DonViVC` text,
+  `MaTheoDoi` text,
+  `NgayDuKienGiaoHang` text
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `baidang`
+--
+ALTER TABLE `baidang`
+  ADD PRIMARY KEY (`MaBD`);
+
+--
+-- Indexes for table `baocao`
+--
+ALTER TABLE `baocao`
+  ADD PRIMARY KEY (`MaBC`);
+
+--
+-- Indexes for table `binhluan`
+--
+ALTER TABLE `binhluan`
+  ADD PRIMARY KEY (`MaBL`);
+
+--
+-- Indexes for table `chitietdonhang`
+--
+ALTER TABLE `chitietdonhang`
+  ADD PRIMARY KEY (`MaCTDH`);
+
+--
+-- Indexes for table `chitietsanpham`
+--
+ALTER TABLE `chitietsanpham`
+  ADD PRIMARY KEY (`MaCTSP`);
+
+--
+-- Indexes for table `danhmucsanpham`
+--
+ALTER TABLE `danhmucsanpham`
+  ADD PRIMARY KEY (`MaDM`);
+
+--
+-- Indexes for table `donhang`
+--
+ALTER TABLE `donhang`
+  ADD PRIMARY KEY (`MaDH`);
+
+--
+-- Indexes for table `giohang`
+--
+ALTER TABLE `giohang`
+  ADD PRIMARY KEY (`MaGH`);
+
+--
+-- Indexes for table `hangsanxuat`
+--
+ALTER TABLE `hangsanxuat`
+  ADD PRIMARY KEY (`MaHSX`);
+
+--
+-- Indexes for table `khuyenmai`
+--
+ALTER TABLE `khuyenmai`
+  ADD PRIMARY KEY (`MaKM`);
+
+--
+-- Indexes for table `lichsutrangthaidh`
+--
+ALTER TABLE `lichsutrangthaidh`
+  ADD PRIMARY KEY (`MaLS`);
+
+--
+-- Indexes for table `phanquyen`
+--
+ALTER TABLE `phanquyen`
+  ADD PRIMARY KEY (`MaPQ`);
+
+--
+-- Indexes for table `phuongthucthanhtoan`
+--
+ALTER TABLE `phuongthucthanhtoan`
+  ADD PRIMARY KEY (`MaPTTT`);
+
+--
+-- Indexes for table `sanpham`
+--
+ALTER TABLE `sanpham`
+  ADD PRIMARY KEY (`MaSP`);
+
+--
+-- Indexes for table `taikhoan`
+--
+ALTER TABLE `taikhoan`
+  ADD PRIMARY KEY (`MaTK`);
+
+--
+-- Indexes for table `thongbao`
+--
+ALTER TABLE `thongbao`
+  ADD PRIMARY KEY (`MaTB`);
+
+--
+-- Indexes for table `thongtinvanchuyen`
+--
+ALTER TABLE `thongtinvanchuyen`
+  ADD PRIMARY KEY (`MaVC`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `baidang`
+--
+ALTER TABLE `baidang`
+  MODIFY `MaBD` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `baocao`
+--
+ALTER TABLE `baocao`
+  MODIFY `MaBC` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `binhluan`
+--
+ALTER TABLE `binhluan`
+  MODIFY `MaBL` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `chitietdonhang`
+--
+ALTER TABLE `chitietdonhang`
+  MODIFY `MaCTDH` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `chitietsanpham`
+--
+ALTER TABLE `chitietsanpham`
+  MODIFY `MaCTSP` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `danhmucsanpham`
+--
+ALTER TABLE `danhmucsanpham`
+  MODIFY `MaDM` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `donhang`
+--
+ALTER TABLE `donhang`
+  MODIFY `MaDH` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `giohang`
+--
+ALTER TABLE `giohang`
+  MODIFY `MaGH` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `hangsanxuat`
+--
+ALTER TABLE `hangsanxuat`
+  MODIFY `MaHSX` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `khuyenmai`
+--
+ALTER TABLE `khuyenmai`
+  MODIFY `MaKM` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `lichsutrangthaidh`
+--
+ALTER TABLE `lichsutrangthaidh`
+  MODIFY `MaLS` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `phanquyen`
+--
+ALTER TABLE `phanquyen`
+  MODIFY `MaPQ` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `phuongthucthanhtoan`
+--
+ALTER TABLE `phuongthucthanhtoan`
+  MODIFY `MaPTTT` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `sanpham`
+--
+ALTER TABLE `sanpham`
+  MODIFY `MaSP` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `thongbao`
+--
+ALTER TABLE `thongbao`
+  MODIFY `MaTB` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `thongtinvanchuyen`
+--
+ALTER TABLE `thongtinvanchuyen`
+  MODIFY `MaVC` int(11) NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
