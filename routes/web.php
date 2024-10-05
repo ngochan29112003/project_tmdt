a9d487d68535baaa3a226b6c0e408a415e978530
ngochan <21004092@st.vlute.edu.vn>
2024-10-05 23:31:32 +0700

    .

--- routes/web.php
+++ routes/web.php
@@ -14,6 +14,4 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    Route::get('/dashboard',[DashBoardController::class,'getViewDashBoard'])->name('index.dashboard');
-});
+Route::get('/dashboard',[DashBoardController::class,'getViewDashBoard'])->name('index.dashboard');
