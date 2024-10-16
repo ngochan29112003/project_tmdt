4d96ea317045a95e839238129de10d9c3be8229f
ngochan <21004092@st.vlute.edu.vn>
2024-10-16 17:48:37 +0700

    .

--- resources/views/super-admin/quan-ly-danh-muc/danh-sach-muc.blade.php
+++ resources/views/super-admin/quan-ly-danh-muc/danh-sach-muc.blade.php
@@ -5,10 +5,25 @@
             <div class = "row g-2 align-items-center">
                 <div class = "col">
                     <h2 class = "page-title">
-                        Quản lý tài khoản admin </h2>
+                        Quản lý danh mục </h2>
                 </div>
             </div>
         </div>
+
+        <!-- lấy này nè -->
+        <div class="row mt-2">
+            <div class="col-md-9 d-flex align-items-center gap-2 justify-content-start">
+                <button class="btn btn-primary d-flex align-items-center" data-bs-toggle="modal" data-bs-target="#Modaldanhmuc">
+                    <i class="bi bi-file-earmark-plus pe-2"></i>
+                    Thêm mới
+                </button>
+                <a href="#" class="btn btn-success d-flex align-items-center text-white btn-export">
+                    <i class="bi bi-file-earmark-arrow-down pe-2"></i>
+                    Xuất file Excel
+                </a>
+            </div>
+        </div>
+
     </div>
 
     <div class = "page-body">
@@ -17,19 +32,19 @@
                 <div class = "col-12">
                     <div class = "card">
                         <div class = "table-responsive p-2">
-                            <table id = "tableBoMon" class = "table table-vcenter card-table table-striped">
+                            <table id = "tableDanhMuc" class = "table table-vcenter card-table table-striped">
                                 <thead>
                                 <tr>
                                     <th>STT</th>
-                                    <th>Tên tài khoản</th>
-                                    <th>Email</th>
-                                    <th>Giới tính</th>
-                                    <th>Ngày sinh</th>
-                                    <th>Địa chỉ</th>
+                                    <th>Tên danh mục</th>
+                                    <th>Ảnh danh mục</th>
+                                    <th>Trạng thái danh mục</th>
                                     <th class = "text-center">Action</th>
                                 </tr>
                                 </thead>
-                                <tbody></tbody>
+                                <tbody>
+
+                                </tbody>
                             </table>
                         </div>
                     </div>
@@ -37,14 +52,49 @@
             </div>
         </div>
     </div>
+
+    <!-- ======= Modal thêm (tìm hiểu Modal này trên BS5) ======= -->
+    <div class="modal fade" id="Modaldanhmuc">
+        <div class="modal-dialog modal-lg"> <!-- Chỉnh thành modal-lg để form rộng hơn -->
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h4 class="modal-title">Thêm danh mục</h4>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                    <form id="Formdanhmuc" enctype="multipart/form-data">
+                        @csrf
+                        <div class="row">
+                            <div class="col-md-6 mb-3">
+                                <label for="TenDM" class="form-label">Tên danh mục</label>
+                                <input type="text" class="form-control" name="TenDM" id="TenDM" required>
+                            </div>
+                            <div class="col-md-6 mb-3">
+                                <label for="AnhDM" class="form-label">Ảnh</label>
+                                <input type="text" class="form-control" name="AnhDM" id="AnhDM" required>
+                            </div>
+                            <div class="col-md-12 mb-3">
+                                <label for="TrangTaiDM" class="form-label">Trạng thái danh mục</label>
+                                <input type="text" class="form-control" name="TrangTaiDM" id="TrangTaiDM" required>
+                            </div>
+                        </div>
+                        <div class="text-end">
+                            <button type="submit" class="btn btn-primary">Thêm</button>
+                            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Hủy</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
 @endsection
 @section('scripts')
     <script>
-      var table = $('#tableBoMon').DataTable({
+      var table = $('#tableDanhMuc').DataTable({
         "language": {
           "emptyTable": "Không có dữ liệu trong bảng",
           "search": "Tìm kiếm:",
-          "lengthMenu": "Hiển thị _MENU_ tài khoản mỗi trang",
+          "lengthMenu": "Hiển thị _MENU_ danh mục mỗi trang",
           "zeroRecords": "Không tìm thấy kết quả",
           "infoEmpty": "Không có dữ liệu"
 
