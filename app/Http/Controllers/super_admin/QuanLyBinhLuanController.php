cd35b1e4dd501b9c921baad72285f681fb3fbc21
Mahuyentran <166974747+Mahuyentran@users.noreply.github.com>
2024-10-18 13:09:18 +0700

    .

--- app/Http/Controllers/super_admin/QuanLyBinhLuanController.php
+++ app/Http/Controllers/super_admin/QuanLyBinhLuanController.php
@@ -4,12 +4,16 @@ namespace App\Http\Controllers\super_admin;
 
 use App\Http\Controllers\Controller;
 
+use App\Models\admin\QuanLyBinhLuan;
 use Illuminate\Http\Request;
 class QuanLyBinhLuanController extends Controller
 {
     public function getView()
     {
-        return view('super-admin.quan-ly-binh-luan.danh-sach-binh-luan');
+        $quanlybinhluanModel = new QuanLyBinhLuan();
+        $list_binh_luan= $quanlybinhluanModel->getbinhluan();
+        return view('super-admin.quan-ly-binh-luan.danh-sach-binh-luan', 
+        compact('list_binh_luan'));
     }
 
 }
