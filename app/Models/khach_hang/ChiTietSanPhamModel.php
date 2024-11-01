71c52d472b2e89a59a6db97bd1b9cfa4e3ee6fa5
Mahuyentran <166974747+Mahuyentran@users.noreply.github.com>
2024-11-01 13:00:40 +0700

    .

--- app/Models/khach_hang/ChiTietSanPhamModel.php
+++ app/Models/khach_hang/ChiTietSanPhamModel.php
@@ -2,12 +2,30 @@
 
 namespace App\Models\khach_hang;
 
+use DB;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class ChiTietSanPhamModel extends Model
 {
     use HasFactory;
+    protected $table="chitietsanpham";
+    protected $primaryKey='MaCTSP';
 
+    //  protected $fillable = [
+    //      'MaDH',
+    //      'MaSP',
+    //      'SoLuong',
+    //      'Gia',
+    // ];
+    
+    public $timestamps = false;
+
+    public function getctsp()
+    {
+        return DB::table('chitietsanpham')
+        ->join('sanpham','sanpham.MaSP','=','chitietsanpham.MaSP')
+        ->get();
+    }
 
 }
